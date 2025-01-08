8d1a9680116463a7a5546a21315f38167c877929
pavelbuchta <pavelbuchta80@gmail.com>
2025-01-08 22:26:06 +0100

    typescript bug fix

--- dist/noketa.d.ts
+++ dist/noketa.d.ts
@@ -1,14 +1,14 @@
-type ProfileAttributes = {
-    first_name?: string;
-    last_name?: string;
-    locale?: string;
-    gender?: string;
-    age?: number;
-    birthdate?: Date;
-    external_id?: string;
-    last_event_date?: Date;
-    properties?: Object;
-};
+export type ProfileAttributes = Partial<{
+    first_name?: string | undefined;
+    last_name?: string | undefined;
+    locale?: string | undefined;
+    gender?: string | undefined;
+    age?: number | undefined;
+    birthdate?: Date | undefined;
+    external_id?: string | undefined;
+    last_event_date?: Date | undefined;
+    properties?: Object | undefined;
+}>;
 export declare class Noketa {
     private apiKey;
     constructor(apiKey: string);
@@ -29,4 +29,3 @@ export declare class Noketa {
         }) => Promise<any>;
     };
 }
-export {};
--- src/noketa.ts
+++ src/noketa.ts
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-type ProfileAttributes = {
-  first_name?: string;
-  last_name?: string;
-  locale?: string;
-  gender?: string;
-  age?: number;
-  birthdate?: Date;
-  external_id?: string;
-  last_event_date?: Date;
-  properties?: Object;
-};
+export type ProfileAttributes = Partial<{
+  first_name?: string | undefined;
+  last_name?: string | undefined;
+  locale?: string | undefined;
+  gender?: string | undefined;
+  age?: number | undefined;
+  birthdate?: Date | undefined;
+  external_id?: string | undefined;
+  last_event_date?: Date | undefined;
+  properties?: Object | undefined;
+}>;
 
 export class Noketa {
   private apiKey: string;
